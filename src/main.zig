8048c53274e268dabfa84019c0b753db8bb40673
Michael Beckwith <mabeckwith@outlook.com>
2025-10-29 20:25:43 -0400

    added a button

--- src/main.zig
+++ src/main.zig
@@ -14,19 +14,37 @@ pub fn main() anyerror!void {
 
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        // get the mouse position
+        const button_rectangle = rl.Rectangle{
+            .x = 350,
+            .y = 200,
+            .width = 100,
+            .height = 50,
+        };
+        var button_color: rl.Color = rl.Color.blue;
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        const mouse_position = rl.getMousePosition();
+        const mouse_over_button = rl.checkCollisionPointRec(mouse_position, button_rectangle);
+        var button_pressed: bool = false;
+
+        if (mouse_over_button) {
+            if (rl.isMouseButtonDown(.left)) {
+                // Button action goes here
+                button_pressed = true;
+                button_color = rl.Color.red;
+            } else {
+                button_color = rl.Color.green;
+            }
+        }
+
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(.white);
+        rl.drawRectangleRec(button_rectangle, button_color);
+        rl.drawText("Click Me!", button_rectangle.x + 15, button_rectangle.y + 15, 14, .black);
 
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
+        // rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
         //----------------------------------------------------------------------------------
     }
 }
